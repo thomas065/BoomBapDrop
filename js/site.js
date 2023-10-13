14cb72aa5badb66287ca3b5382b7da51e05fde1d
Thomas Bell <thomasjbell065@gmail.com>
2023-10-13 11:40:33 -0400

    adding story elements

--- js/site.js
+++ js/site.js
@@ -15,3 +15,34 @@ function displayMessage() {
         }
     );
 }
+
+// get the start and end numbers from the page 
+function getPageNumbers() {
+    let 
+
+}
+
+// generate a list of all numbers between the start and end
+function listNumbers() {
+
+}
+
+// display each of those numbers on the page
+function displayNumbers() {
+
+}
+
+ // with each number, put it on the page
+
+
+
+
+
+
+// given 2 values display the numbers 1-100
+
+// when a number is divisible by the first value, display "Boom"
+
+// When a number is divisible by the second value, display "Bap"
+
+// when a number is divisible by both values, display "BoomBap...Drop!"
