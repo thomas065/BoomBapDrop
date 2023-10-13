edf06d7a300454f768f0a018445425d1a31b02ab
Thomas Bell <thomasjbell065@gmail.com>
2023-10-13 16:30:24 -0400

    got the code working, still needs work

--- js/site.js
+++ js/site.js
@@ -1,45 +1,83 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('secondNote').value;
+// function displayMessage() {
+//     // get the text out of our <input> and assign it to a variable
+//     let msg = document.getElementById('secondNote').value;
 
-    // alert('Hello Guardian Cohort!')
-    Swal.fire(
-        {
-            icon: 'success',
-            backdrop: false,
-            title: `<strong>BoomBap!</strong>`,
-            text: `It's a SMASH hit!`, // <---- replace this text with that variable
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-        }
-    );
-}
+//     // alert('Hello Guardian Cohort!')
+// }
 
-// get the start and end numbers from the page 
+// get users input
 function getNotes() {
-    let firstNote = getElementById('firstNote').value;
+    let firstNote = document.getElementById('firstNote').value;
 
-    let secondNote = getElementById('secondNote').value;
+    firstNote = parseInt(firstNote);
 
+    if (Number.isInteger(firstNote)) {
+        let generatedNotes = generateNotes(firstNote);
+
+        displayNotes(generatedNotes);
+
+    //     Swal.fire({
+    //         backdrop: false,
+    //         title: `<strong>BoomBap!</strong>`,
+    //         text: `It's a SMASH hit!`, // <---- replace this text with that variable
+    //         imageUrl: 'https://unsplash.it/400/200',
+    //         imageWidth: 400,
+    //         imageHeight: 200,
+    //         imageAlt: 'Custom image',
+    //         timerProgressBar: true,
+    //     });
+    // } else {
+    //     Swal.fire({
+    //         icon: 'error',
+    //         title: 'Yikes, that hurt my ears',
+    //         backdrop: false,
+    //     });
+    }
 }
 
-// generate a list of all notes between the first and second
-function generateNotes(first, second) {
+// generate a list of all notes
+function generateNotes(first) {
+    let notes = [];
 
+    for (let n = first; n <= 100; n++) {
+        if (n % 3 == 0) {
+            notes.push('Boom')
+        } else {
+            notes.push(n)
+        }
+    }
+    return notes;
 }
 
-// display each of those numbers on the page
-function displayNumbers() {
+// display the notes on the page
+function displayNotes(notes) {
 
+    let html = "";
+
+    for (let i = 0; i < notes.length; i++) {
+    let currentNote = notes[i];
+
+    // if (currentNote % 3 == 0) {
+    //     if (currentNote % 5 == 0) {
+    //         'BoomBap'
+    //     } else {
+    //         'Bap'
+    //     }
+    // } else {
+    //     if (currentNote % 5 == 0) {
+    //         'Boom'
+    //     } else {
+    //         currentNote
+    //     }
+    // }
+    html += `<tr><td>${currentNote}</td></tr>`
 }
 
- // with each number, put it on the page
+    let tbody = document.getElementById('results');
+    tbody.innerHTML = html;
+}
 
-
-
-
-
+// with each number, put it on the page
 
 // given 2 values display the numbers 1-100
 
